8b504c482b443ec250f44c24745d970f7e3f29d6
Nathan Konigkramer <98358837+Ven1M@users.noreply.github.com>
2022-06-01 18:01:34 +1200

    Create Registration_class.cs

---
+++ Registration_class.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Registration 
+{
+  public class Registration
+  {
+        private SqlConnection Conn = new SqlConnection();
+        private SqlCommand Cmd = new SqlCommand();
+        private SqlDataReader Reader_Login;
+        string QueryString;
+        
+        Public Regisration()
+        {
+        
+        
+        }
+        
+  }
+  
+}
